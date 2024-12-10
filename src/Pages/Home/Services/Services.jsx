85370812ac09e1d7a34628583b55d4ce2c34b8a8
Afran1984 <afranrazzak1984@gmail.com>
2024-12-10 09:59:20 +0600

    Service Update

--- src/Pages/Home/Services/Services.jsx
+++ src/Pages/Home/Services/Services.jsx
@@ -1,9 +1,54 @@
-import React from 'react';
 
 const Services = () => {
     return (
         <div>
-            <h1>Services</h1>
+            <h2 className="text-3xl text-center">Our Services</h2>
+        <div className="grid grid-cols-3 p-6 gap-5">
+            <div className="card glass w-96">
+        <figure>
+          <img
+            src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+            alt="car!" />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title">Life hack</h2>
+          <p>How to park your car at your garage?</p>
+          <div className="card-actions justify-end">
+            <button className="btn btn-primary">Learn now!</button>
+          </div>
+        </div>
+      </div>
+      {/* 02 */}
+      <div className="card glass w-96">
+        <figure>
+          <img
+            src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+            alt="car!" />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title">Life hack</h2>
+          <p>How to park your car at your garage?</p>
+          <div className="card-actions justify-end">
+            <button className="btn btn-primary">Learn now!</button>
+          </div>
+        </div>
+      </div>
+      {/* 03 */}
+      <div className="card glass w-96">
+        <figure>
+          <img
+            src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+            alt="car!" />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title">Life hack</h2>
+          <p>How to park your car at your garage?</p>
+          <div className="card-actions justify-end">
+            <button className="btn btn-primary">Learn now!</button>
+          </div>
+        </div>
+      </div>
+        </div>
         </div>
     );
 };
