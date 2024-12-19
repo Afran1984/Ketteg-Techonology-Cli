7592449b77dda872482bbcfb94dc9f98584c017f
Afran1984 <afranrazzak1984@gmail.com>
2024-12-20 00:10:41 +0600

    update about & contact

--- src/Pages/Home/Abouts/Abouts.jsx
+++ src/Pages/Home/Abouts/Abouts.jsx
@@ -2,8 +2,17 @@ import React from 'react';
 
 const Abouts = () => {
     return (
-        <div>
+        <div className='p-5'>
             <h1>About for Ketteg techonology</h1>
+            <ul className="steps w-full">
+                <li data-content="2023" className="step step-neutral">Idea generate</li>
+                <li data-content="" className="step step-neutral">Step 2</li>
+                <li data-content="✓" className="step step-neutral">Step 3</li>
+                <li data-content="✕" className="step step-neutral">Step 4</li>
+                <li data-content="★" className="step step-neutral">Step 5</li>
+                <li data-content="" className="step step-neutral">Step 6</li>
+                <li data-content="●" className="step step-neutral">Step 7</li>
+            </ul>
         </div>
     );
 };
--- src/Pages/Home/Contact/Contact.jsx
+++ src/Pages/Home/Contact/Contact.jsx
@@ -3,7 +3,46 @@ import React from 'react';
 const Contact = () => {
     return (
         <div>
-            <h1>Conatct: +8801717663447</h1>
+            <div>
+                <h1 className='text-5xl text-center mt-10'>Tell us how we can help</h1>
+                <div className="divider"></div>
+                <div className='grid grid-cols-2 mt-3 gap-10 p-5'>
+                    <div>
+                        <h1 className='text-3xl'>Just drop us a line with some description of what you are looking for and we will take it from there.</h1>
+                        <p className='mt-10'>btw, if you want to join us then please follow the instructions on Company & Culture page and email your CV to hrkettegtech@gmail.com</p>
+                    </div>
+                    <div>
+                        <div>
+                        <label className='-mt-3' htmlFor="Email">Email Address</label> <br />
+                        {/* <input type="text" placeholder="Type here" className="mt-2 input input-bordered w-full max-w-xs" /> */}
+                        <input type="text" placeholder='Enter Your Email' className='w-full p-3 border border-indigo-600' required />
+                        </div>
+                        <div className='mt-10'>
+                            <label className='-mt-3' htmlFor="">Message</label> <br />
+                            {/* <textarea className="textarea textarea-primary" placeholder="Bio"></textarea> */}
+                            <textarea className='w-full p-3 h-32 border border-indigo-600 ' name="" placeholder='Enter your Message' id=""></textarea>
+                        </div>
+                        <button className='border border-indigo-600 p-2 mt-4'>Submit</button>
+                    </div>
+                </div>
+                <div className="divider"></div>
+                <div className='grid grid-cols-2 p-5'>
+                    <div>
+                        <h1 className='text-3xl uppercase'>Chittagong</h1>
+                        <h1 className='text-2xl mt-2'>Mozaffer Nagar Residential Area</h1>
+                        <p className='text-2xl mt-2'>Profrssor Village, 3rd floor</p>
+                        <h1 className='text-2xl mt-2'>Polytechnic</h1>
+                        <h1 className='text-2xl mt-2'>Bayejid Bostami</h1>
+                        <h1 className='text-2xl mt-2'>Chittagong 4209</h1>
+                        <h1 className='text-2xl mt-2'>+8801717663447</h1>
+                        <p className='text-2xl mt-2'>kettegtech@gmail.com</p>
+
+                    </div>
+                    <div>
+                        <p>Google Map</p>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
